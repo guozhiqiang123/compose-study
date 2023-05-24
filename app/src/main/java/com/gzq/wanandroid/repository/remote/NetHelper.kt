fc045587108e8538354b742e6f48ef033d29cfb8
gzq <gzq@yoc.tech>
2023-05-24 22:21:38 +0800

    重命名不规范的变量

--- app/src/main/java/com/gzq/wanandroid/repository/remote/NetHelper.kt
+++ app/src/main/java/com/gzq/wanandroid/repository/remote/NetHelper.kt
@@ -1,5 +1,6 @@
 package com.gzq.wanandroid.repository.remote
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.gzq.wanandroid.repository.WanAndroidApi
@@ -14,30 +15,32 @@ import retrofit2.converter.gson.GsonConverterFactory
 object NetHelper {
     const val BASE_URL = "https://www.wanandroid.com"
 
-    private lateinit var _retrofit: Retrofit
+    private lateinit var retrofit: Retrofit
 
-    private lateinit var _httpClient: OkHttpClient
+    private lateinit var httpClient: OkHttpClient
 
     lateinit var service: WanAndroidApi
 
+    //因为引入的是Application级别的上下文，这里忽略内存泄露
+    @SuppressLint("StaticFieldLeak")
     lateinit var networkHandler: NetworkHandler
 
 
     fun init(context: Context) {
-        _httpClient = OkHttpClient.Builder()
+        httpClient = OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE))
             .addInterceptor(ChuckerInterceptor.Builder(context).build())
             .addInterceptor(SetCookieInterceptor())
             .addInterceptor(CacheCookieInterceptor())
             .build()
 
-        _retrofit = Retrofit.Builder()
-            .client(_httpClient)
+        retrofit = Retrofit.Builder()
+            .client(httpClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        service = _retrofit.create(WanAndroidApi::class.java)
+        service = retrofit.create(WanAndroidApi::class.java)
 
         networkHandler = NetworkHandler(context)
     }
